54fce8613507190736b6559b6b9092205b85d917
reotendo <35835779+reotendo@users.noreply.github.com>
2025-12-15 14:54:06 +0900

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>AddVenture</h1>
+      <p>未知を開拓する初めの一歩を</p>
+      <p>🚀 デプロイ成功！</p>
+    </main>
+  );
+}
